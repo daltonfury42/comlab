6e025b0e1fee5e1dfeae6c324854b5c682ab5740
Dalton Fury <daltonfury42@gmail.com>
2017-03-23 11:43:26 +0530

    Improved gen.sh

--- simpleCompiler/gen.sh
+++ simpleCompiler/gen.sh
@@ -1,5 +1,8 @@
-lex simCom.l && lex -P lt labelTranslate.l && yacc -d simCom.y && gcc -g symbolTable.c  exptree.c codeGen.c lex.yy.c lex.lt.c y.tab.c labelTranslate.c && ./a.out input
+lex simCom.l && lex -P lt labelTranslate.l && yacc -d simCom.y && gcc -g symbolTable.c  exptree.c codeGen.c lex.yy.c lex.lt.c y.tab.c labelTranslate.c  -g && ./a.out input
+if [ $? -eq 0 ] 
+then
 echo "Running the simulator with comiled code:"
 cd xsm_expl/
 ./xsm -e ../xsm.out
 cd ..
+fi
